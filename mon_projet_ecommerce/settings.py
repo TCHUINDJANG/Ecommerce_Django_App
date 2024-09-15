f26fb4f640d822545014454c407855f41d2f76a4
Tchuindjang <paulnicolas519@gmail.com>
2024-09-16 01:18:57 +0200

    connexion avec la base de donnee

--- mon_projet_ecommerce/settings.py
+++ mon_projet_ecommerce/settings.py
@@ -87,24 +87,23 @@ WSGI_APPLICATION = 'mon_projet_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_HOST'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST':config('DB_HOST'),
-#         'PORT':config('DB_PORT',default='5432')
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 
+DATABASES = {
+    'default' : {
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME': 'Ecommerce',
+        'USER':'postgres',
+        'PASSWORD':'Davide2020@@',
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
 
 
 # Password validation
